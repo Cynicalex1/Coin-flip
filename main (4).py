192cab177819d20adcea002764ae277f259289fe
Cynicalex1 <144472846+Cynicalex1@users.noreply.github.com>
2023-09-08 12:39:41 -0400

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,13 @@
+#Problem 3.2.3 - Copy and paste link to Canvas once the tests are passed.
+import random
+
+list = ["H" , "T"]
+for i in range (1,00000):
+  other = random.choice(list)
+  print(other)
+  if other == "H":
+    print(other)
+ # else:
+    #print(other)
+
+#its not done but I'll continue to keep working on it during the summer until I figure a way to do it.
